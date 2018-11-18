6fcd7d790dd79996cc9f8b820e77175bc4c36e78
Mary Jack <maryjackinthebox@gmail.com>
2018-11-18 22:00:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 
+class Driver {
+  constructor (name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+   yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+}
+ class Route {
+  constructor (beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+   blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'MadisonAvenue', '5th Avenue'];
+     let endHorizontal     = eastWest.indexOf(this.endingLocation.horizontal);
+    let beginningHorizontal = eastWest.indexOf(this.beginningLocation.horizontal);
+     let horizontalDist = Math.abs(endHorizontal - beginningHorizontal);
+    let verticalDist   =  Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
+     let totalDist = horizontalDist + verticalDist;
+     return totalDist;
+    }
+     estimatedTime(peakHours) {
+      let timeTravelled
+       if (peakHours) {
+        timeTravelled = this.blocksTravelled() / 2;
+      } else {
+        timeTravelled = this.blocksTravelled() / 3;
+      }
+       return timeTravelled;
+    }
+}
